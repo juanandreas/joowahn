8602f26191a3d84170c19f0aaeaac24097d6ff87
Juan Andreas <joowahn97@gmail.com>
2019-07-04 14:41:30 -0700

    Updates

---
+++ precache-manifest.d34b16294e69814c0a5f1ad5cd53b2a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447488d15ff40860cfdd52a75bc2c11c",
+    "url": "/joowahn/index.html"
+  },
+  {
+    "revision": "50ac16b23b42c59a7f59",
+    "url": "/joowahn/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/joowahn/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "50ac16b23b42c59a7f59",
+    "url": "/joowahn/static/js/main.a46ea944.chunk.js"
+  },
+  {
+    "revision": "cefa8ad269b4ac2a0956",
+    "url": "/joowahn/static/js/runtime~main.84d177a8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/joowahn/static/media/logo.5d5d9eef.svg"
+  }
+]);
