f5dcec55102df5661571c3273482c538b7c8af59
Juan Andreas <joowahn97@gmail.com>
2019-07-05 20:22:53 -0700

    arc reactor

--- src/App.js
+++ src/App.js
@@ -6,7 +6,29 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
+      <div class="fullpage-wrapper">
+        <div class="reactor-container">
+          <div class="reactor-container-inner circle abs-center"></div>
+          <div class="tunnel circle abs-center"></div>
+          <div class="core-wrapper circle abs-center"></div>
+          <div class="core-outer circle abs-center"></div>
+          <div class="core-inner circle abs-center"></div>
+          <div class="coil-container">
+            <div class="coil coil-1"></div>
+            <div class="coil coil-2"></div>
+            <div class="coil coil-3"></div>
+            <div class="coil coil-4"></div>
+            <div class="coil coil-5"></div>
+            <div class="coil coil-6"></div>
+            <div class="coil coil-7"></div>
+            <div class="coil coil-8"></div>
+          </div>
+        </div>
+      </div>
+
+
         <p>
           Welcome to my website.
           <br></br>
